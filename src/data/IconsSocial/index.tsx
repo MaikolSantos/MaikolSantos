7b3280d43a569625eca6ae689e556ee0ae515709
Maikol Santos <maikol.lourencon@gmail.com>
2023-08-09 23:40:35 -0300

    fix: remove social youtube

--- src/data/IconsSocial/index.tsx
+++ src/data/IconsSocial/index.tsx
@@ -17,10 +17,6 @@ export const iconsSocial: iconsProps[] = [
     href: "https://www.linkedin.com/in/maikol-lourencon/",
   },
   {
-    icon: <FaYoutube color="#C1CBD3" size={40} />,
-    href: "https://www.youtube.com/@maikolsants",
-  },
-  {
     icon: <FaWhatsapp color="#C1CBD3" size={40} />,
     href: "https://api.whatsapp.com/send?phone=5516981988786",
   },
