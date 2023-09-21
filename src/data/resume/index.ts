59e89b9845765bf844a5e216e9696bea869c8765
Maikol Santos <maikol.lourencon@gmail.com>
2023-09-21 10:43:48 -0300

    fix: correct experrtise

---
+++ src/data/resume/index.ts
@@ -0,0 +1,28 @@
+export const resume = [
+  {
+    paragraph:
+      "Experiência em desenvolvimento Front-End web utilizando HTML, CSS, SASS, Javascript, Typescript, React, Next.js e consumo de API's",
+  },
+  {
+    paragraph:
+      "Experiência em desenvolvimento Back-End utilizando Node.js, Express, TypeORM, Python, Django, SQL, PostgreSQL",
+  },
+  {
+    paragraph:
+    "Experiência com Git e Github; metodologias ágeis Scrum e Kanban e UI/UX Designer utilizando Figma",
+  },
+  {
+    paragraph:
+      "Atuei em uma fintech como desenvolvedor front-end, onde desenvolvi habilidades como subordinado, colaborador e desenvolvedor",
+  },
+  {
+    paragraph:
+      "Participei ativamente de projetos em equipe, contribuindo para a criação de soluções criativas e inovadoras para problemas complexos. Em alguns desses projetos, tive a oportunidade de liderar a equipe, o que me permitiu comprovar minhas habilidades técnicas e competências com as tecnologias e metodologias mencionadas",
+  },
+  {
+    paragraph: "Inglês básico",
+  },
+  {
+    paragraph: "Espanhol intermediário",
+  },
+];
--- src/pages/components/Sections/Resume/index.tsx
+++ src/pages/components/Sections/Resume/index.tsx
@@ -8,6 +8,7 @@ import {
 } from "./styles";
 
 import universe from "../../../../assets/images/universe.jpg";
+import { resume } from "../../../../data/resume";
 
 const Resume = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -31,49 +32,16 @@ const Resume = () => {
               <div>
                 <h3>Experiências</h3>
                 <ul>
-                  <li>
-                    <p>
-                      Experiência em desenvolvimento Front-End web utilizando
-                      HTML, CSS, SASS, Javascript, Typescript, React, Next.js e
-                      consumo de API's;
-                    </p>
-                  </li>
-                  <li>
-                    <p>
-                      Experiência em desenvolvimento Back-End utilizando
-                      Node.js, Express, TypeORM, Python, Django, SQL,
-                      PostgreSQL;
-                    </p>
-                  </li>
-                  <li>
-                    <p>
-                      Familiaridade com Git, Github, metodologias ágeis Scrum e
-                      Kanban;
-                    </p>
-                  </li>
-                  <li>
-                    <p>
-                      Atuei em uma fintech como desenvolvedor front-end, onde
-                      desenvolvi habilidades como subordinado, colaborador e
-                      desenvolvedor;
-                    </p>
-                  </li>
-                  <li>
-                    <p>
-                      Participei ativamente de projetos em equipe, contribuindo
-                      para a criação de soluções criativas e inovadoras para
-                      problemas complexos. Em alguns desses projetos, tive a
-                      oportunidade de liderar a equipe, o que me permitiu
-                      comprovar minhas habilidades técnicas e competências com
-                      as tecnologias e metodologias mencionadas;
-                    </p>
-                  </li>
-                  <li>
-                    <p>Inglês básico;</p>
-                  </li>
-                  <li>
-                    <p>Espanhol intermediário.</p>
-                  </li>
+                  {resume.map(({ paragraph }, index) => {
+                    return (
+                      <li key={index}>
+                        <p>
+                          {paragraph +
+                            (index !== resume.length - 1 ? ";" : ".")}
+                        </p>
+                      </li>
+                    );
+                  })}
                 </ul>
               </div>
             </section>
